f7d2bc39d67531d54e1b347b00a21bdfbf7e9bd3
Daresamson <165600918+Daresamson@users.noreply.github.com>
2025-03-10 14:24:08 -0700

    Update PROJECT2.tf

--- PROJECT2.tf
+++ PROJECT2.tf
@@ -1,1 +1,33 @@
 
+THIS IS MAIN.tf
+
+module "s3-static-website" {
+  source  = "cn-terraform/s3-static-website/aws"
+  version = "1.0.9"
+
+  bucket_name    = "your-unique-bucket-name"  # Replace with your unique bucket name
+  index_document = "index.html"                 # The entry point of your static website
+}
+
+
+
+
+
+
+
+
+
+# THIS IS PROVIDER.TF
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2" # Replace with your desired region
+}
